291f500bd166628c1b881cfb23546870ae963500
Hritujeet <132835698+Hritujeet@users.noreply.github.com>
2025-01-21 21:15:41 +0530

    Fix Bugs

--- icoder/src/app/blogs/page.js
+++ icoder/src/app/blogs/page.js
@@ -12,7 +12,7 @@ const Blogs = async () => {
         <section className="flex flex-col justify-center items-center">
             <div className="text-white px-10 w-full mb-10 bg-zinc-900 h-[40vh] flex flex-col justify-center items-center">
                 <h1 className="text-center my-5 texttext-3xl md:text-5xl font-bold">Read Programming Blogs</h1>
-                {a.sessionId && <Link href={"/blogs/create/someID"}><Button size={"lg"} className={"font-semibold text-lg"}>Craft a
+                {a.sessionId && <Link href={`/blogs/create/${a.sessionId}`}><Button size={"lg"} className={"font-semibold text-lg"}>Craft a
                     Blog</Button></Link>}
             </div>
             <div className="w-[90vw] md:w-[60vw] px-4 py-2 flex flex-col gap-4">
