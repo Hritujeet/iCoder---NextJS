f127fdca033b057d320bc3f8d23a807a8e237092
Hritujeet <132835698+Hritujeet@users.noreply.github.com>
2025-01-25 12:15:31 +0530

    Fixed Bugs and Made UI Improvements

--- icoder/src/components/Posts/PostContainer.js
+++ icoder/src/components/Posts/PostContainer.js
@@ -2,7 +2,7 @@
 import React from "react";
 import {useQuery} from "@tanstack/react-query";
 import {Skeleton} from "@/components/ui/skeleton";
-import PostCard from "@/components/ui/PostCard";
+import PostCard from "@/components/Posts/PostCard";
 
 const PostContainer = () => {
     const {data, isLoading} = useQuery({
